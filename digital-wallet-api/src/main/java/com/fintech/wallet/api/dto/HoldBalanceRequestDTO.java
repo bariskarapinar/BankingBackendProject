437000cc1cda9bb043ed56df99363753ce17ea1f
bariskarapinar <bkarapinar1@hotmail.com>
2026-09-14 14:43:32 +0300

    Balance Update

---
+++ digital-wallet-api/src/main/java/com/fintech/wallet/api/dto/HoldBalanceRequestDTO.java
@@ -0,0 +1,21 @@
+package com.fintech.wallet.api.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class HoldBalanceRequestDTO {
+    @NotBlank(message = "Amount is required")
+    @Positive(message = "Amount must be positive")
+    private String amount;
+
+    @NotBlank(message = "Hold reference is required")
+    private String holdReference;
+}
